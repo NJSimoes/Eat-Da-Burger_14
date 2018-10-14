710e45cd8f09437e258fc454fb44849e8286e547
Nuno Simoes <nunosimoesemail@gmail.com>
2018-10-14 16:56:27 -0400

    update

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,11 @@
+// 2. Create a `connection.js` file inside `config` directory.
+
+//    * Inside the `connection.js` file, setup the code to connect Node to MySQL.
+
+//    * Export the connection.
+
+
+
 var mysql = require("mysql");
 
 var connection;
